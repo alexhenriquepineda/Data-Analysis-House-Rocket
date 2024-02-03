e3caaa058aaeaec4d8a16c3d4f18c004d4c30686
Alex Pineda <allexhenrique@gmail.com>
2024-02-03 15:19:46 -0300

    feat(text): add data dictionary

--- app/text_file.py
+++ app/text_file.py
@@ -14,6 +14,30 @@ introduction = """
     So, welcome to our exploratory journey into the real estate market of Seattle!
     """
 
+
+dictionary = """
+Data Dictionary:
+        id: Unique identifier for each property
+        price: Price of the property in dollars
+        bedrooms: Number of bedrooms in the property
+        bathrooms: Number of bathrooms in the property
+        sqft_living: Total living space area in square feet
+        sqft_lot: Total lot area in square feet
+        floors: Number of floors in the property
+        waterfront: Binary indicator (0 or 1) for waterfront property
+        view: Rating of the property's view (typically from 0 to 4)
+        condition: Overall condition rating of the property (typically from 1 to 5)
+        grade: Overall grade given to the housing unit, based on King County grading system
+        sqft_above: Area of the property above ground level in square feet
+        sqft_basement: Area of the property's basement in square feet
+        yr_built: Year the property was built
+        yr_renovated: Year of the last renovation
+        zipcode: Zip code of the property location
+        lat: Latitude coordinate of the property
+        long: Longitude coordinate of the property
+        """
+
+
 target_analysis = """
     In these graphs, it is evident that numerous outliers exist in the price attribute. Additionally, there is an asymmetric distribution to the left, characterized by a high concentration of data falling within the range of $300,000 to $600,000
     """
